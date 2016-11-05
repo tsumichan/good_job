1594d91b81192a8343f6aacc59b3052109b684a8
tsumi <kdy78022@gmail.com>
2016-11-05 18:22:17 +0900

    mail fixed

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -15,7 +15,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
   	@contact = contact
 
-  	mail to: "kdy78022@gmail.com",
+  	mail to:  @contact.email ,
   	subject: '【Achieve】お問い合わせが完了しました！'
   end
 end
